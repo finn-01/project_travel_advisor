19e8f69066bf1fd14c37aede29cc8f00692b21f1
finn-01 <nguyensam576@gmail.com>
2022-01-28 23:14:17 +0700

    error

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@mui/material";
+import AccessAlarm from "@mui/icons-material/AccessAlarm";
 
-import "./style.css";
+import useStyle from "./style";
 
 const Header = () => {
-	return <div>Header</div>;
+	const classes = "a";
+
+	return (
+		<AppBar position="static">
+			<Toolbar>
+				<Typography variant="h5">Travel Advisor</Typography>
+				<Box display="flex">
+					<Typography variant="h6">Explore new places</Typography>
+					{/* <Autocomplete> */}
+					<div>
+						<div>
+							<AccessAlarm />
+						</div>
+						<InputBase placeholder="Search..." />
+					</div>
+					{/* </Autocomplete> */}
+				</Box>
+			</Toolbar>
+		</AppBar>
+	);
 };
 
 export default Header;
